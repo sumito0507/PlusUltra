34670f77c5f228033076c38ca4000fd97eab1c9a
sumito <sakura.mille.feuille@gmail.com>
2018-12-02 16:00:44 +0900

    GPSサンプル

---
+++ Samples/GPSSample/GPSSample/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace GPSSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbLatitude = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbLongitude = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbLatitude
+            // 
+            this.tbLatitude.Location = new System.Drawing.Point(118, 105);
+            this.tbLatitude.Name = "tbLatitude";
+            this.tbLatitude.Size = new System.Drawing.Size(182, 22);
+            this.tbLatitude.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Latitude";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Longitude";
+            // 
+            // tbLongitude
+            // 
+            this.tbLongitude.Location = new System.Drawing.Point(118, 154);
+            this.tbLongitude.Name = "tbLongitude";
+            this.tbLongitude.Size = new System.Drawing.Size(182, 22);
+            this.tbLongitude.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 353);
+            this.Controls.Add(this.tbLongitude);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbLatitude);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Global Positioning Satellite";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbLatitude;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbLongitude;
+    }
+}
+
---
+++ Samples/GPSSample/GPSSample/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Device.Location;
+
+namespace GPSSample
+{
+    public partial class Form1 : Form
+    {
+        private GeoCoordinateWatcher _wtc = new GeoCoordinateWatcher();
+        public Form1()
+        {
+            InitializeComponent();
+
+            _wtc.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(wtc_PositionChanged);
+            //_wtc.Start();
+            _wtc.TryStart(false, TimeSpan.FromMilliseconds(1000));
+        }
+
+        void wtc_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            try
+            {
+
+                if (e.Position.Location.IsUnknown != true)
+                {
+                    this.tbLatitude.Text = e.Position.Location.Latitude.ToString();
+                    this.tbLongitude.Text = e.Position.Location.Longitude.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.tbLatitude.Text = _wtc.Position.Location.Latitude.ToString();
+            this.tbLongitude.Text = _wtc.Position.Location.Longitude.ToString();
+        }
+    }
+}
