b97dc5b15769513b58d6f470c77e374f6c79f226
sakura.mille.feuille@gmail.com <sakura.mille.feuille@gmail.com>
2023-06-05 14:46:56 +0900

    〇Windowsフォーム（MVVM）
    ・ボタン制御追加

---
+++ Samples/WindowsFormsMVVM/WindowsFormMVVM2/Command/BaseICommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WindowsFormMVVM2.Command
+{
+    public class BaseICommand : ICommand
+    {
+        public delegate void DelegateAction();
+        private DelegateAction _delegateAction;
+
+        public event EventHandler CanExecuteChanged;
+
+        public BaseICommand(Action action)
+        {
+            _delegateAction = new DelegateAction(action);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _delegateAction();
+        }
+    }
+}
---
+++ Samples/WindowsFormsMVVM/WindowsFormMVVM2/Command/SampleButtonCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WindowsFormMVVM2.Command
+{
+    class SampleButtonCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private readonly Action _commandAction;
+        private readonly Func<bool> _canExecuteCommandAction;
+
+        public SampleButtonCommand(Action commandAction, Func<bool> canExecuteCommandAction = null)
+        {
+            _commandAction = commandAction;
+            _canExecuteCommandAction = canExecuteCommandAction;
+        }
+
+        public bool CanExecute(object parameter) => _canExecuteCommandAction?.Invoke() ?? true;
+        public void Execute(object parameter) => _commandAction.Invoke();
+        public void NotifyCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
